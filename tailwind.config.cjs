a03e9e47a9e058143f8b7aa177d7706ce8d82fed
rickyrazhari <rickyraihan83@gmail.com>
2022-10-05 15:41:47 +0700

    add braga and tictactoe

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['"Poppins", sans-serif'],
+        exo: ['"Exo2", sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
